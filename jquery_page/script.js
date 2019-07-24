a75b845f5cc74d72ed2a2fa775c7a8a91fea94f4
Danstan Onyango <danstan.otieno@gmail.com>
2019-07-25 00:36:03 +0300

    adds jquery smaple code

---
+++ jquery_page/script.js
@@ -0,0 +1,15 @@
+function populateDate(){
+    var months = ["January","February","March","April","May", "June","July","August","September","October","November","December"]
+    $("#month").text(months[new Date().getMonth()]);
+    $("#day").text(new Date().getDate());
+    $("#year").text(new Date().getFullYear());
+    $("#hour").text(new Date().getHours());
+    $("#minute").text(new Date().getMinutes());
+    $("#seconds").text(new Date().getSeconds());
+}
+populateDate();
+
+$("#refresh").click(function(){
+    populateDate();
+});
+
